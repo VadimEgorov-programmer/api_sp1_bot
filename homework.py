bc3986edbb2d6b952bdd6409c5be6275c3288d1d
Vadim <wadim-egoroff@yandex.ru>
2020-10-31 20:44:09 +0300

    Done

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import requests
 import telegram
@@ -6,30 +7,44 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    try:
+        homework_name = homework.get('homework_name')
+        if homework_name is None:
+            raise Exception('This name is missing')
+        homework_status = homework.get('status')
+        if homework_status not in ('approved', 'rejected'):
+            raise Exception('status errors')
+        if homework_status == 'rejected':
+            verdict = 'К сожалению в работе нашлись ошибки.'
+        else:
+            verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    except Exception as e:
+        logging.exception(f'error values {e}')
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
-    return homework_statuses.json()
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
+    params = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(URL, params=params, headers=headers)
+        return homework_statuses.json()
+    except Exception as e:
+        logging.exception(f'error {e}')
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
